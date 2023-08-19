6198ff003338eb2483e0afb7f59d1081288da926
Yoshimoto Sho <s.yoshimoto@enjoyworks.jp>
2023-08-19 10:59:15 +0900

    create intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+const App = ({message}: {message: string}) => {
+  return <div>{ message }</div>;
 };
 
 export default App;
