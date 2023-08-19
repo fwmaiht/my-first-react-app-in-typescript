d32c0f0c537331abd0765e70a442a39dbafde81d
Yoshimoto Sho <s.yoshimoto@enjoyworks.jp>
2023-08-19 11:34:17 +0900

    basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const App = ({message}: {message: string}) => {
+interface AppProps {
+  message?: string;
+}
+
+const App: React.FunctionComponent<AppProps> = ({message}) => {
   return <div>{ message }</div>;
+};
+
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
 };
 
 export default App;
